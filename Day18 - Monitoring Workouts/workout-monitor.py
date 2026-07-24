46c521e53906eaa7510660161d54cb7b42b07f26
SUJITH PEDAPATI <pedapatisujith@gmail.com>
2026-07-24 18:24:34 +0530

    F:\D Drive\Linkedin Learning\Open CV, Hands on AI\Basics of Open CV\Day18 - Monitoring Workouts

---
+++ Day18 - Monitoring Workouts/workout-monitor.py
@@ -0,0 +1,35 @@
+import cv2
+from ultralytics import solutions
+
+cap = cv2.VideoCapture(r"F:\D Drive\Linkedin Learning\Open CV, Hands on AI\Basics of Open CV\Day18 - Monitoring Workouts\video.mov")
+w,h,fps = (
+    int(cap.get(x)) 
+    for x in (cv2.CAP_PROP_FRAME_WIDTH,
+              cv2.CAP_PROP_FRAME_HEIGHT,
+              cv2.CAP_PROP_FPS)
+)
+
+video_writer = cv2.VideoWriter(
+    "result.avi",
+    cv2.VideoWriter_fourcc(*"mp4v"),
+    fps,(w,h)
+)
+
+## Workout Model Implementation
+solution = solutions.AIGym(
+    model = "yolov8n-pose.pt",
+    kpts = [6,8,10],
+    show = True,
+    line_width = 3
+)
+
+
+while cap.isOpened():
+    success,im0 = cap.read()
+    if not success : 
+        break
+    results = solution(im0)
+    video_writer.write(results.plot_im)
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
